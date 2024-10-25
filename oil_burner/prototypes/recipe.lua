ee0c0f9c377d4371446ac5f8398ff2c8d6b2dd5a
toxicantidote <23189273+toxicantidote@users.noreply.github.com>
2024-10-26 01:07:34 +1030

    Updates to comply with more explicit modding API
    Apparently the modding API is more explicit now. Refactored entity code to build from oil-refinery entity instead.

--- oil_burner/prototypes/recipe.lua
+++ oil_burner/prototypes/recipe.lua
@@ -12,102 +12,102 @@ data:extend({
 		enabled = false,
 		ingredients =
             {
-            {"steel-plate", 20},
-            {"iron-gear-wheel", 10},
-            {"stone-brick", 10},
-            {"pipe", 10}
+            {type = "item", name = "steel-plate", amount = 20},
+            {type = "item", name = "iron-gear-wheel", amount = 10},
+            {type = "item", name = "stone-brick", amount = 10},
+            {type = "item", name = "pipe", amount = 10}
             },
 		energy_required = 5,
-		result = "oil-burner",
+		results = {{type = "item", name = "oil-burner", amount = 1}}
 	},
     
     {
-    type = "recipe",
-    name = "oil-burner-crude",
-    category = "oil-burner-recipe",
-    enabled = true,
-    hide_from_player_crafting = true,
-    energy_required = 30,
-    ingredients =
-    {
-        {type="fluid", name="crude-oil", amount=25}
-    },
-    results = {},
-    icon = "__base__/graphics/icons/fluid/crude-oil.png",
-    icon_size=64,
-    subgroup = "fluid-recipes",
-    order = "b[fluid-chemistry]-b[crude-oil]",
-    },
-    
-    {
-    type = "recipe",
-    name = "oil-burner-light",
-    category = "oil-burner-recipe",
-    enabled = true,
-    hide_from_player_crafting = true,
-    energy_required = 30,
-    ingredients =
-    {
-        {type="fluid", name="light-oil", amount=25}
-    },
-    results = {},
-    icon = "__base__/graphics/icons/fluid/light-oil.png",
-    icon_size=64,
-    subgroup = "fluid-recipes",
-    order = "b[fluid-chemistry]-b[light-oil]",
+        type = "recipe",
+        name = "oil-burner-crude",
+        category = "oil-burner-recipe",
+        enabled = true,
+        hide_from_player_crafting = true,
+        energy_required = 30,
+        ingredients =
+        {
+            {type="fluid", name="crude-oil", amount=25}
+        },
+        results = {},
+        icon = "__base__/graphics/icons/fluid/crude-oil.png",
+        icon_size=64,
+        subgroup = "fluid-recipes",
+        order = "b[fluid-chemistry]-b[crude-oil]",
     },
     
     {
-    type = "recipe",
-    name = "oil-burner-heavy",
-    category = "oil-burner-recipe",
-    enabled = true,
-    hide_from_player_crafting = true,
-    energy_required = 30,
-    ingredients =
-    {
-        {type="fluid", name="heavy-oil", amount=25}
-    },
-    results = {},
-    icon = "__base__/graphics/icons/fluid/heavy-oil.png",
-    icon_size=64,
-    subgroup = "fluid-recipes",
-    order = "b[fluid-chemistry]-b[heavy-oil]",
-    },
-    
-    {
-    type = "recipe",
-    name = "oil-burner-petroleum",
-    category = "oil-burner-recipe",
-    enabled = true,
-    hide_from_player_crafting = true,
-    energy_required = 30,
-    ingredients =
-    {
-        {type="fluid", name="petroleum-gas", amount=25}
-    },
-    results = {},
-    icon = "__base__/graphics/icons/fluid/petroleum-gas.png",
-    icon_size=64,
-    subgroup = "fluid-recipes",
-    order = "b[fluid-chemistry]-b[petroleum-gas]",
+        type = "recipe",
+        name = "oil-burner-light",
+        category = "oil-burner-recipe",
+        enabled = true,
+        hide_from_player_crafting = true,
+        energy_required = 30,
+        ingredients =
+        {
+            {type="fluid", name="light-oil", amount=25}
+        },
+        results = {},
+        icon = "__base__/graphics/icons/fluid/light-oil.png",
+        icon_size=64,
+        subgroup = "fluid-recipes",
+        order = "b[fluid-chemistry]-b[light-oil]",
     },
     
     {
-    type = "recipe",
-    name = "oil-burner-lubricant",
-    category = "oil-burner-recipe",
-    enabled = true,
-    hide_from_player_crafting = true,
-    energy_required = 30,
-    ingredients =
-    {
-        {type="fluid", name="lubricant", amount=25}
+        type = "recipe",
+        name = "oil-burner-heavy",
+        category = "oil-burner-recipe",
+        enabled = true,
+        hide_from_player_crafting = true,
+        energy_required = 30,
+        ingredients =
+        {
+            {type="fluid", name="heavy-oil", amount=25}
+        },
+        results = {},
+        icon = "__base__/graphics/icons/fluid/heavy-oil.png",
+        icon_size=64,
+        subgroup = "fluid-recipes",
+        order = "b[fluid-chemistry]-b[heavy-oil]",
     },
-    results = {},
-    icon = "__base__/graphics/icons/fluid/lubricant.png",
-    icon_size=64,
-    subgroup = "fluid-recipes",
-    order = "b[fluid-chemistry]-b[lubricant]",
+    
+    {
+        type = "recipe",
+        name = "oil-burner-petroleum",
+        category = "oil-burner-recipe",
+        enabled = true,
+        hide_from_player_crafting = true,
+        energy_required = 30,
+        ingredients =
+        {
+            {type="fluid", name="petroleum-gas", amount=25}
+        },
+        results = {},
+        icon = "__base__/graphics/icons/fluid/petroleum-gas.png",
+        icon_size=64,
+        subgroup = "fluid-recipes",
+        order = "b[fluid-chemistry]-b[petroleum-gas]",
+    },
+    
+    {
+        type = "recipe",
+        name = "oil-burner-lubricant",
+        category = "oil-burner-recipe",
+        enabled = true,
+        hide_from_player_crafting = true,
+        energy_required = 30,
+        ingredients =
+        {
+            {type="fluid", name="lubricant", amount=25}
+        },
+        results = {},
+        icon = "__base__/graphics/icons/fluid/lubricant.png",
+        icon_size=64,
+        subgroup = "fluid-recipes",
+        order = "b[fluid-chemistry]-b[lubricant]",
     }
 })
